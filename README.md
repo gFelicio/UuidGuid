3a61c82e3760e5cfa9cd7b2ed0673c8295d30417
gFelicio <gustavo.a.felicio@gmail.com>
2018-01-12 12:18:52 -0200

    second function implementation &mentation && readme update

--- README.md
+++ README.md
@@ -1,23 +1,20 @@
 # UuidGuid
 
-Functions written in PHP that generates UUIDS. 
+Functions written in PHP that generates UUIDS.
 
 ------------------------------------------------------------------------------------------------------------------------------------------
 
 The first function, which was created by Andrew Moore in http://www.php.net/manual/en/function.uniqid.php#94959 , is in compliance with RFC 4122.
 
-The second function in the file generates a pseudo-random, "pseudo-version4-uuid".
-There are a few positions in a version four UUID that should be filled with pre-defined characters, but the second function generates random numbers for all positions in the UUID.
-
 -----------------------------------------------------------------------------------------------------------------------------------------
 
 # CONSIDERATIONS
 
-The functions were developed to work in plain PHP projects, but might work with PHP Frameworks, like Laravel.
+The function was developed to work in plain PHP projects, but might work with PHP Frameworks, like Laravel.
 
-Since the functions uses rand(), mt_rand() or similar functions to generate the UUIDs, the randomness of the generated UUIDs are NOT GUARANTEED TO BE UNIQUE.
+Since the function uses rand(), mt_rand() or similar functions to generate the UUIDs, the randomness of the generated UUIDs are NOT GUARANTEED TO BE UNIQUE.
 
-If your project just demands a random generated number to fill an UUID number, but you don't need it to be an unique key number, feel free to use these functions.
+If your project just demands a random generated number to fill an UUID number, but you don't need it to be an unique key number, feel free to use this function.
 
 If your projects demand uniqueness and "cryptographical" key fields, there is no guarantee that these functions will generate safe UUIDs.
 
--- uuid.php
+++ uuid.php
@@ -1,41 +1,25 @@
 <?php
-    /**
-        *----------------------------------------------------------------------------------------
-        * UUID class
-        *
-        * @author Andrew Moore
-        * @link http://www.php.net/manual/en/function.uniqid.php#94959
-        *
-        *----------------------------------------------------------------------------------------
+
+    class UUID
+    {
+        /*
+        * ------------------------------------------------------------------------------------------------------
+        * Version 4 UUIDs are pseudo-random
+        * ------------------------------------------------------------------------------------------------------
         *  Usage
-        * ---------------------------------------------------------------------------------------
-        *
+        * ------------------------------------------------------------------------------------------------------
         * include 'UUID.php';
-        *
-        * ---------------------------------------------------------------------------------------
-        *
+        * ------------------------------------------------------------------------------------------------------
         * Pseudo-random UUID
-        * $v4uuid = UUID::v4();
+        * $uuid = UUID::v4();
         *
         *       OR
         *
         * UUID::v4();
         * $uuid = UUID::v4();
         *
-        *
         * http://php.net/manual/en/language.oop5.static.php
-        *
-        * ---------------------------------------------------------------------------------------
-    */
-
-    class UUID
-    {
-        /*
-            *----------------------------------------------------------------------------------
-            * Generate v4 UUID
-            *----------------------------------------------------------------------------------
-            * Version 4 UUIDs are pseudo-random.
-            *----------------------------------------------------------------------------------
+        * ------------------------------------------------------------------------------------------------------
         */
 
         public static function v4()
@@ -61,6 +45,63 @@
         {
             return preg_match('/^\{?[0-9a-f]{8}\-?[0-9a-f]{4}\-?[0-9a-f]{4}\-?'.
             '[0-9a-f]{4}\-?[0-9a-f]{12}\}?$/i', $uuid) === 1;
+        }
+
+        /*
+        * ------------------------------------------------------------------------------------------------------
+        *  Using /dev/random
+        * ------------------------------------------------------------------------------------------------------
+        *  Usage
+        * ------------------------------------------------------------------------------------------------------
+        * include 'UUID.php';
+        * ------------------------------------------------------------------------------------------------------
+        * Pseudo-random UUID
+        * $uuid = UUID::uuidSecure();
+        *
+        *       OR
+        *
+        * UUID::uuidSecure();
+        * $uuid = UUID::uuidSecure();
+        * ------------------------------------------------------------------------------------------------------
+        */
+
+        public static function uuidSecure() {
+
+            $pr_bits = null;
+            $fp = @fopen('/dev/urandom','rb');
+            if ($fp !== false) {
+                $pr_bits .= @fread($fp, 16);
+                @fclose($fp);
+            } else {
+                $this->cakeError('randomNumber');
+            }
+
+            $time_low = bin2hex(substr($pr_bits,0, 4));
+            $time_mid = bin2hex(substr($pr_bits,4, 2));
+            $time_hi_and_version = bin2hex(substr($pr_bits,6, 2));
+            $clock_seq_hi_and_reserved = bin2hex(substr($pr_bits,8, 2));
+            $node = bin2hex(substr($pr_bits,10, 6));
+
+            /**
+             * Set the four most significant bits (bits 12 through 15) of the
+             * time_hi_and_version field to the 4-bit version number from
+             * Section 4.1.3.
+             * @see http://tools.ietf.org/html/rfc4122#section-4.1.3
+             */
+            $time_hi_and_version = hexdec($time_hi_and_version);
+            $time_hi_and_version = $time_hi_and_version >> 4;
+            $time_hi_and_version = $time_hi_and_version | 0x4000;
+
+            /**
+             * Set the two most significant bits (bits 6 and 7) of the
+             * clock_seq_hi_and_reserved to zero and one, respectively.
+             */
+            $clock_seq_hi_and_reserved = hexdec($clock_seq_hi_and_reserved);
+            $clock_seq_hi_and_reserved = $clock_seq_hi_and_reserved >> 2;
+            $clock_seq_hi_and_reserved = $clock_seq_hi_and_reserved | 0x8000;
+
+            return sprintf('%08s-%04s-%04x-%04x-%012s',
+                $time_low, $time_mid, $time_hi_and_version, $clock_seq_hi_and_reserved, $node);
         }
     }
 ?>
